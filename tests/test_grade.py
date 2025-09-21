f22afd7c6fbd2db0b53aa9db6b7dd618562f9885
Stas Khirman <stas@khirman.com>
2025-09-21 12:38:51 -0400

    remove pd.set_option('future.no_silent_downcasting', True) due lack of supports in versions !=2.2

--- tests/test_grade.py
+++ tests/test_grade.py
@@ -34,7 +34,7 @@ def test_grade_iglu_r_compatible(scenario):
     expected_results = scenario["results"]
     expected_df = pd.DataFrame(expected_results)
     expected_df = expected_df.reset_index(drop=True)
-    pd.set_option('future.no_silent_downcasting', True)
+    # pd.set_option('future.no_silent_downcasting', True)
     expected_df = expected_df.replace({None: np.nan})
 
 
--- tests/test_quantile_glu.py
+++ tests/test_quantile_glu.py
@@ -33,7 +33,7 @@ def test_quantile_glu_iglu_r_compatible(scenario):
     expected_results = scenario["results"]
     expected_df = pd.DataFrame(expected_results)
     expected_df = expected_df.reset_index(drop=True)
-    pd.set_option('future.no_silent_downcasting', True)
+    # pd.set_option('future.no_silent_downcasting', True)
     expected_df = expected_df.replace({None: np.nan})
 
 
